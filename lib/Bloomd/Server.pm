0abee5e9ee25dedfae02699394c668a8cdb14356
Keiji, Yoshimi <walf443@gmail.com>
2011-10-29 14:00:47 +0900

    initial commit

---
+++ lib/Bloomd/Server.pm
@@ -0,0 +1,33 @@
+package Bloomd::Server;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Bloomd::Server -
+
+=head1 SYNOPSIS
+
+  use Bloomd::Server;
+
+=head1 DESCRIPTION
+
+Bloomd::Server is
+
+=head1 AUTHOR
+
+Keiji Yoshimi E<lt>walf443 at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
